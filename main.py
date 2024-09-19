ca4c8588e9068c81ed497a14e75fed49168a6059
petrov <petrov.dmitrii@phystech.edu>
2024-09-19 22:18:46 +0300

    Добавил разбиение по месяцам и статистику

--- main.py
+++ main.py
@@ -72,11 +72,35 @@ def volumes(pool, securities, date_from, date_till):
                                                                      <= dates[-1]) else dates[-1]
             candle_df = to_df(get_candles(pool, security, start_date.isoformat(), end_date.isoformat(), 24))
             df.loc[df['Ticker'] == security, [value.split(' ')[0] for value in candle_df['begin'].values]] = \
-                                                                                    candle_df['value'].values
+                candle_df['value'].values
             start_date = end_date + datetime.timedelta(days=1)
         start_date = dates[0]
         df = df.dropna(axis=1, how='all')
     return df
+
+
+def extract_month(df, year, month):
+    cols = [df.columns.tolist()[0]] + [strdate for strdate in df.columns.tolist()[1:] if
+                                       datetime.date.fromisoformat(
+                                           strdate).year == year and datetime.date.fromisoformat(
+                                           strdate).month == month]
+    month_df = df[cols]
+    return month_df
+
+def divide_months(df, start_date, end_date):
+    monthes = [(date.year, date.month) for date in pd.date_range(start_date, end_date, freq='ME')]
+    extracts = []
+    if monthes[-1] != (end_date.year, end_date.month):
+        monthes.append((end_date.year, end_date.month))
+    for year, month in monthes:
+        extracts.append((year, month, extract_month(df, year, month)))
+    return extracts
+
+def add_stats(df):
+    with_stats = df.copy()
+    with_stats['Средний объем'] = df.iloc[:, 1:].mean(axis=1)
+    with_stats['Среднеквадратичное отклонение'] = df.iloc[:, 1:].std(axis=1)
+    return with_stats
 
 if __name__ == '__main__':
     http = urllib3.PoolManager()
@@ -85,5 +109,14 @@ if __name__ == '__main__':
     all = all_securities(http, 500)
     securities = list(filter(lambda x: len(x) < 6, all))
 
-    df = volumes(http, MOEX, date_from='2024-01-01', date_till='2024-09-17')
-    df.to_excel('volumes.xlsx')
+    start_date = datetime.date.fromisoformat('2023-01-01')
+    end_date   = datetime.date.fromisoformat('2024-09-18')
+    month_delta = (end_date.year - start_date.year)
+    df = volumes(http, securities, date_from=start_date, date_till=end_date)
+    divided_months = divide_months(df, start_date, end_date)
+    df = add_stats(df)
+    with pd.ExcelWriter('volumes_all.xlsx') as writer:
+        df.to_excel(writer, sheet_name='Все дни')
+        for year, month, month_df in divided_months:
+            month_df = add_stats(month_df)
+            month_df.to_excel(writer, sheet_name='{0:d}-{1:d}'.format(year, month))
